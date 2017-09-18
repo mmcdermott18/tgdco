3d0505a4f950986ba551136e4beec8f861e36e15
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2017-09-18 18:31:10 -0400

    chuging along
    chuging along

--- cao/js/main.js
+++ cao/js/main.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
       
     });
     // run function on scroll
-    $(window).scroll(function {
+    $(window).scroll(function() {
 
     });
 });
 function slideOuts() {
   $('#a-1').click(function() {
-    console.log('test');
     $( document ).on( 'keydown', function ( e ) {
       if ( e.keyCode === 27 ) {
         $('#a-1-info').fadeOut();
+        $('body').removeClass('scrollLock');
       }
     });
     $('#a-1-info').fadeIn();
-    // $('body').toggleClass('scrollLock');
+    $('body').toggleClass('scrollLock');
   });
 }
