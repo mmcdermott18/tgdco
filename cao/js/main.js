68c2e51df14726076b689b4f33428bf459cf3bbc
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2017-09-20 14:33:23 -0400

    full prototype v1

--- cao/js/main.js
+++ cao/js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
     slideOuts();
+    breakHeight();
     // run function on resize of the window
     $(window).resize(function(){
       
     });
     // run function on scroll
     $(window).scroll(function() {
-
+      breakHeight();
     });
 });
 function slideOuts() {
@@ -20,4 +21,19 @@ function slideOuts() {
     $('#a-1-info').fadeIn();
     $('body').toggleClass('scrollLock');
   });
+}
+function breakHeight() {
+  var h = $('.page-break-img').outerHeight();
+  var wh = $(window).height();
+  var hh = $('header').outerHeight();
+  var nh = (wh - hh);
+  console.log('img height ' + h);
+  console.log('window height ' + wh);
+  if (h > wh) {
+    $('.page-break').css('height', nh);
+    console.log('page break set as ' + nh);
+  } else {
+    $('.page-break').css('height',h);
+    console.log('page break set as ' + h);
+  }
 }
