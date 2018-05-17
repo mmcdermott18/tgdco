5c26eaf727f382bb21e2b09e19fbfd168eb7923f
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2018-05-17 15:15:15 -0400

    Making it more fun?

--- maren/js/main.js
+++ maren/js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
     animate();
+    newPhrase();
     // run function on resize of the window
     $(window).resize(function(){
      
@@ -16,4 +17,23 @@ function animate() {
 	$('#mark').click(function(){
 	    $('#mark').toggleClass('ani-trans');
 	});
+}
+function newPhrase() {
+    var phrases = [
+    'Be in the know.',
+    'Keep your ear to the ground.',
+    'Laurel or Yanny?',
+    'Keep it real.',
+    'Your future self will thank you.',
+    'We only send good emails.',
+    'Living by the water isn\'t appealing?']
+    var number = Math.floor(Math.random() * (phrases.length));
+    var duration = Math.floor(Math.random() * 10000);
+    console.log(phrases[number]);
+    console.log(duration);
+    $('#phraseInput').text(phrases[number]);
+    setTimeout(function () {
+        $('#phraseInput').text(phrases[number]);
+        newPhrase();
+    }, duration)
 }
