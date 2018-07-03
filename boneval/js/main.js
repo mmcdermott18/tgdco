2051093c153666b6d151adb3b1b13f72fa2a5bc6
mmcdermott18 <mmcdermott18@users.noreply.github.com>
2018-07-03 10:00:11 -0400

    boneval initial

---
+++ boneval/js/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+  logo();
+  slide();
+    // run function on resize of the window
+    $(window).resize(function(){
+      logo();
+    });
+    // run function on scroll
+    $(window).scroll(function() {
+
+    });
+});
+
+function logo() {
+  var lH = $('#desktop-logo').outerHeight();
+  var wH = $('#rendering').outerHeight();
+  var lO = ((wH - lH)/2);
+  $('#desktop-logo').css('top', lO);
+}
+
+function slide() {
+  $("#show-all").click(function() {
+    $("#left").toggleClass("left-animate");
+    $("#rendering").toggleClass("render-animate");
+    $("#desktop-logo").toggleClass("fade");
+    var show = "See Full Rendering";
+    var hide = "Back to Site";
+    var txt = $("#button-text").text();
+    console.log('txt is ' + txt);
+    if (txt == show) {
+      $("#button-text").text(hide);
+    } else {
+      $("#button-text").text(show);
+    }
+  }); 
+}
