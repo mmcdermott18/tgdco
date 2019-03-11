57e32586c6ae7fd0f23e835095504d3b16c43eb8
Michael McDermott <mmcdermott18@gmail.com>
2019-03-10 22:40:59 -0400

    Image slider in header

--- themaren/js/main.js
+++ themaren/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     waves();
     logoClick();
     slider();
+    slideshow()
     // run function on resize of the window
     $(window).resize(function(){
 
@@ -45,5 +46,33 @@ function slider() {
       speed: 1000,
       slideMargin:0,
       loop:true
+  });
+}
+function slideshow() {
+  var slides = ["img/background.jpg", "img/1.jpg", "img/2.jpg", "img/3.jpg", "img/4.jpg"];
+  var i = 0;
+  function slide() {
+    console.log('i is ' + i);
+    $("#tophalf").stop().animate({opacity: 0},1500,function(){
+      $("#tophalf").css({'background-image': "url(" + slides[i] + ")"}).animate({opacity: 1},{duration:1500});
+    });
+  }
+  $('#leftarrow').click(function() {
+    i--;
+    if (i < 0 ) {
+      i = 4;
+      slide();
+    } else {
+      slide();
+    }
+  });
+  $('#rightarrow').click(function() {
+    i++;
+    if (i > 4 ) {
+      i = 0;
+      slide();
+    } else {
+      slide();
+    }
   });
 }
